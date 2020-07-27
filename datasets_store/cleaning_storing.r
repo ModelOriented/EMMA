10e78c47c0d079c4c25ad3406618bd3c8307c686
okcze <piotr.phitz@gmail.com>
2020-07-27 14:29:34 +0200

    Preprocessing and information base

--- datasets_store/cleaning_storing.r
+++ datasets_store/cleaning_storing.r
@@ -5,6 +5,8 @@ library(dplyr)
 library(janitor)
 library(tidyselect)
 
+source("./datasets_store/create_new_summary.r")
+
 #Reading list of selected data sets
 datasets <- read_csv("./datasets_store/datasets_selection/selected_datasets.csv")
 
@@ -13,14 +15,16 @@ check_type <- function(x){
 }
 
 data_types_troubles <- c()
+miss_in_target <- c()
 
-for(id in datasets$ID){
+#Temporary test on 20 datasets
+for(id in datasets$ID[1:20]){
   
   df_oml <- getOMLDataSet(id)
   df_desc <- df_oml$desc
   df <- df_oml$data
   
-  ### Pre-processing step ###
+  ### Preprocessing step ###
   
   #Columns to ignore according to tags
   to_ignore <- df_desc$ignore.attribute
@@ -62,21 +66,22 @@ for(id in datasets$ID){
   df <- remove_empty(df, "cols")
   
   #All to lowercase
-  
-  #ID columns detected manually
+  df <- mutate_if(df, is.factor, function(x) factor(tolower(x)))
   
   #Missing values in target variable
+  target <- df_oml$target.features
+  if(any(is.na(df[, target]))){
+    miss_in_target <- c(miss_in_target, id)
+    warning(paste("Missing values in target variable ID: ", as.character(id)))
+  }
   
   #Categorical columns with high fraction of unique values
+  #?
   
-  #### Collecting information step ###
+  ### Collecting information step ###
+  json_summary <- create_summary(df_oml, df_desc, df)
+  write(json_summary, paste("./datasets_store/information_base/dataset_", id, ".json", sep = ""))
 }
 
-  
-#temp
-df <- t
-x <- c(NA,NA)
-y <- c(2,3)
-z <- c("c", "a")
-t <- data.frame(x,y,z, stringsAsFactors = T)
+#example_json <- fromJSON("./datasets_store/information_base/dataset_25.json")
 
