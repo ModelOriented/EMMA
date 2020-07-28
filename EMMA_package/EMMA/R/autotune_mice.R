727ae1c043f5f815c1de853acf3b0329e261dca2
jjanborowka <janborowka7@gmail.com>
2020-07-28 12:16:53 +0200

    missForest

--- EMMA_package/EMMA/R/autotune_mice.R
+++ EMMA_package/EMMA/R/autotune_mice.R
@@ -1,3 +1,135 @@
+#' Creating formula
+#'
+#' @description This function create formula to use in with function form mice package.
+#' Also inform if its necessary to use gml instead of lm (no numeric values in dataset).
+#'
+#' @param df data.frame. Df to impute with column names and without target column.
+#' @param col_miss character vector. Names of columns with NA.
+#' @param col_no_miss character vector. Names of columns without NA.
+#' @param col_type character vector. Vector containing column type names.
+#' @param percent_of_missing numeric vector. Vector contatining percent of missing data in columns for example  c(0,1,0,0,11.3,..)
+#' @usage formula_creating(df,coll_miss,coll_no_miss,coll_type,percent_of_missing)
+#' @return List with formula object[1] and information if its no numeric value in dataset[2].
+
+
+formula_creating <- function(df,col_miss,col_no_miss,col_type,percent_of_missing){
+
+
+  # Flags if no numeric value in df
+  no_numeric <-  T
+
+  #If df contains numeric values
+  if  ('numeric' %in% col_type | 'intiger' %in% col_type){
+    no_numeric <- F
+    numeric_columns <- colnames(df)[ifelse('numeric' == col_type | 'intiger' == col_type,T,F)]
+
+    #If some numeric columns don't contain missing data
+    numeric_no_missing <- intersect(numeric_columns,colnames(df)[percent_of_missing==0])
+    if (length(numeric_no_missing)>0){
+      predicted_value <- numeric_no_missing[1]
+      if (sum(percent_of_missing>0)>=3){
+        columns_missing  <-  as.data.frame(cbind(percent_of_missing,colnames(df)))
+        columns_missing <- columns_missing[order(as.numeric(as.character(columns_missing$percent_of_missing)),decreasing = TRUE),]
+        predicting_values <- columns_missing$V2[1:3]
+      }
+      else{ predicting_values <- col_miss}
+
+    }
+
+    else{
+      columns_missing_type <- as.data.frame(cbind(percent_of_missing,colnames(df),col_type))
+      columns_missing_type_n_i <- columns_missing_type[columns_missing_type$coll_type=='numeric' | columns_missing_type$coll_type == 'initger',]
+      if (length(row.names(columns_missing_type_n_i))>=1) {
+        predicted_value <- columns_missing_type_n_i[order(columns_missing$percent_of_missing),][1]}
+      else {no_numeric <-  T }
+      if (length(row.names(columns_missing_type[-1,]))>=3){
+        predicting_values <-  columns_missing_type[order(as.numeric(as.character(columns_missing_type$percent_of_missing)),decreasing = T),V2][1:3]
+      }
+      else{predicting_value <-setdiff(predicted_value,col_miss)}
+    }
+
+
+  }
+  # If df don't contains numeric values
+  if (no_numeric){
+    predicted_value <- col_no_miss[1]
+    if (sum(percent_of_missing>0)>=3){
+      columns_missing  <-  as.data.frame(cbind(percent_of_missing,colnames(df)))
+      columns_missing <- columns_missing[order(as.numeric(as.character(columns_missing$percent_of_missing)),decreasing = TRUE),]
+      predicting_values <- columns_missing$V2[1:3]
+    }
+    else{ predicting_values <- col_miss}
+  }
+
+
+
+  return(list(as.formula(paste(as.character(predicted_value),paste(as.character(predicting_values),collapse = '+'),sep='~')),no_numeric))
+
+}
+
+
+
+#' Performing randomSearch for selecting best method and correlation or fraction of features used to create prediction matrix.
+#'
+#' @description This function perform random search and return values corresponding to best mean IMF (missing information fraction).
+#'
+#' @param low_corr double between 0,1 default 0 lower boundry of correlation set.
+#' @param up_corr double between 0,1 default 1 upper boundary of correlation set. Both of these parameters work the same for a fraction of features.
+#' @param methods_random set of methods to chose. Default 'pmm'.
+#' @param df data frame to input.
+#' @param formule first product of formula_creating() funtion. For example formula_creating(...)[1]
+#' @param no_numeric second product of formula_creating() function.
+#' @param iter number of iteration for randomSearch.
+#' @param random.seed radnom seed.
+#' @param correlation If True correlation is using if Fales fraction of features. Default True.
+#'
+#' @details  Even if correlation is set at False correlation its still use to select best features. That mean problem with
+#' calculating correlation between categorical columns is still important.
+#'
+#' @return List with best correlation (or fraction ) at first place, best method at second, and results of every iteration at 3.
+
+random_param_mice_search <- function(low_corr=0,up_corr=1,methods_random = c('pmm'),df,formula,no_numeric,iter,random.seed=123,correlation=T){
+
+  set.seed(random.seed)
+  corr <- runif(iter,0,1)
+  met <- sample(methods_random,iter,replace = T)
+
+
+  # Performing random search and saving result
+  result <- rep(1,iter)
+
+  for (i in 1:iter){
+    skip_to_next <- F
+
+    tryCatch(
+      {
+        if (correlation){
+          inputation <- mice(df,method = met[i],pred=quickpred(df, mincor=corr[i],method = 'spearman'),seed = random.seed)}
+        if (!correlation){
+          inputation <- mice(df,method = met[i],pred=quickpred(df, minpuc=corr[i],method = 'spearman'),seed = random.seed)
+        }
+
+        if (as.logical(no_numeric[1])){
+          fit <- with(inputation,glm(as.formula(as.character(formula)),family = binomial))
+        }
+        if (!as.logical(no_numeric[1])){
+
+          fit <- with(inputation,expr = lm((as.formula(as.character(formula)))))
+        }
+        result[i] <- mean(tidy(pool(fit))$fmi)
+
+      }, error = function(e) { skip_to_next <<- TRUE})
+    if(skip_to_next) { next }
+
+  }
+
+  # Returning result
+  return(list(corr[which.min(result)],met[which.min(result)],result))
+
+}
+
+
+
 #' Automatical tuning of parameters and imputation using mice package.
 #'
 #' @description Function impute missing data using mice functions. First perform random search using linear models (generalized linear models if only
--- EMMA_package/EMMA/R/missForest_single.R
+++
@@ -1,9 +0,0 @@
-library(doParallel)
-library(missForest)
-autotune_missForest <-function(df,percent_of_missing){}
-
-
-
-
-
-
