f9825bf14a12bfdf82aa95bdeb516670ce2ced3b
okcze <piotr.phitz@gmail.com>
2020-08-04 11:52:07 +0200

    scrapping scripts update

--- datasets_store/cleaning_storing.r
+++ datasets_store/cleaning_storing.r
@@ -1,4 +1,6 @@
 #Pre-processing data sets and saving information about them
+#Currently adapted to update JSON files with new elements
+
 library(OpenML)
 library(readr)
 library(dplyr)
@@ -11,10 +13,12 @@ source("./datasets_store/preprocess.R")
 #Reading list of selected data sets
 datasets <- read_csv("./datasets_store/datasets_selection/selected_datasets.csv")
 
-data_types_troubles <- c()
-miss_in_target <- c()
+### Currently developing only binary classification datasets ###
+datasets <- datasets[datasets$task_type == "binary", ]
 
-i <- 1
+#data_types_troubles <- c()
+#miss_in_target <- c()
+
 for(id in datasets$ID){
   
   df_oml <- getOMLDataSet(id)
@@ -35,22 +39,23 @@ for(id in datasets$ID){
   cleaned_df <- preprocess(df, df_desc, df_oml)
   df <- cleaned_df$df
   
-  if(cleaned_df$data_types_troubles){
-    write.table( id,  
-                 file="./datasets_store/information_base/data_types_troubles.csv", 
-                 append = T, 
-                 sep=',', 
-                 row.names=F, 
-                 col.names=F )
-  }
-  if(cleaned_df$miss_in_target){
-    write.table( id,  
-                 file="./datasets_store/information_base/miss_in_target_troubles.csv", 
-                 append = T, 
-                 sep=',', 
-                 row.names=F, 
-                 col.names=F )
-  }
+  # Currently skipping steps which needed to be checked once
+  # if(cleaned_df$data_types_troubles){
+  #   write.table( id,  
+  #                file="./datasets_store/information_base/data_types_troubles.csv", 
+  #                append = T, 
+  #                sep=',', 
+  #                row.names=F, 
+  #                col.names=F )
+  # }
+  # if(cleaned_df$miss_in_target){
+  #   write.table( id,  
+  #                file="./datasets_store/information_base/miss_in_target_troubles.csv", 
+  #                append = T, 
+  #                sep=',', 
+  #                row.names=F, 
+  #                col.names=F )
+  # }
   
   ### Collecting information step ###
   summary <- create_summary(df_oml, df_desc, df)
@@ -60,20 +65,15 @@ for(id in datasets$ID){
   write(json_summary, paste("./datasets_store/information_base/dataset_", id, ".json", sep = ""))
   write.csv(missings_pattern, paste("./datasets_store/patterns_base/dataset_", id, ".csv", sep = ""))
   
-  #Cleaning cache
-  i <- i+1
-  if(i==20){
-    clearOMLCache()
-    i <- 1
-  }
 }
 
+
 #Troubles detection (founded missings in target variable, no problems with data types)
-detected_miss_in_target <- read_csv("datasets_store/information_base/miss_in_target_troubles.csv")
+#detected_miss_in_target <- read_csv("datasets_store/information_base/miss_in_target_troubles.csv")
 
 #Removing from selected data sets
-selected_datasets <- read_csv("./datasets_store/datasets_selection/selected_datasets.csv")
-selected_datasets <- selected_datasets[!selected_datasets$ID %in% detected_miss_in_target$X1, ]
+#selected_datasets <- read_csv("./datasets_store/datasets_selection/selected_datasets.csv")
+#selected_datasets <- selected_datasets[!selected_datasets$ID %in% detected_miss_in_target$X1, ]
 #write.csv(selected_datasets, "./datasets_store/datasets_selection/selected_datasets.csv")
 # for (id in detected_miss_in_target$X1) {
 #   file.remove(paste("./datasets_store/information_base/dataset_", id, ".json", sep = ""))
