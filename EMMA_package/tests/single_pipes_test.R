33ec14e8257f082eac54cd9c342bfe217f8929a0
okcze <piotr.phitz@gmail.com>
2020-08-25 16:05:55 +0200

    start of testing

---
+++ EMMA_package/tests/single_pipes_test.R
@@ -0,0 +1,55 @@
+#Packages
+#devtools::install_github("https://github.com/ModelOriented/EMMA/", subdir = "EMMA_package/EMMA")
+library(EMMA)
+library(mlr3)
+library(mlr3pipelines)
+library(paradox)
+library(OpenML)
+library(mlr3learners)
+library(mlr3oml)
+
+### Simple tests on single pipes
+task1 <- getOMLTask(task.id = 13)
+task2 <- getOMLTask(task.id = 3793)
+task3 <- getOMLTask(task.id = 3667)
+
+for (i in c(13, 3793, 3667)) {
+try({
+#Take pipe
+pipe_imp <- EMMA::PipeOpVIM_IRMI$new()
+#Check params and docs
+# ?PipeOpVIM_IRMI
+# pipe_imp$param_set
+
+#Build simple model
+pipe_model <- lrn("classif.xgboost")
+pipe_encoding <- PipeOpEncodeImpact$new()
+graph <- pipe_imp %>>% pipe_encoding %>>% pipe_model
+graph_learner <- GraphLearner$new(graph)
+split <- rsmp("holdout")
+
+#Test
+f <- file("EMMA_package/tests/logs/vim_irmi.txt", open = "a")
+task <- mlr3oml::OMLTask$new(id = i)
+task <- task$task
+
+sink(f, type = "output")
+sink(f, type = "message")
+options(warn = 1)
+
+rr <- resample(task, graph_learner, split)
+
+if(exists("rr")){
+write(file = f, "\n")
+rr$print()
+write(file = f, "\n")
+rr$score()
+}
+
+write(file = f, "\n\n")
+sink()
+sink()
+rm(list = ls())
+})
+}
+
