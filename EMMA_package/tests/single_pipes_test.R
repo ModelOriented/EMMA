d749c9635dc9246c7711293dceef75b5214662f7
okcze <piotr.phitz@gmail.com>
2020-08-26 14:12:38 +0200

    further tests

--- EMMA_package/tests/single_pipes_test.R
+++ EMMA_package/tests/single_pipes_test.R
@@ -14,22 +14,23 @@ task2 <- getOMLTask(task.id = 3793)
 task3 <- getOMLTask(task.id = 3667)
 
 for (i in c(13, 3793, 3667)) {
-try({
+
 #Take pipe
-pipe_imp <- EMMA::PipeOpVIM_IRMI$new()
+pipe_imp <- EMMA::PipeOpMice$new()
 #Check params and docs
-# ?PipeOpVIM_IRMI
+# ?PipeOpMice
+#?PipeOpmissRanger
 # pipe_imp$param_set
 
 #Build simple model
-pipe_model <- lrn("classif.xgboost")
+pipe_model <- lrn("classif.glmnet")
 pipe_encoding <- PipeOpEncodeImpact$new()
 graph <- pipe_imp %>>% pipe_encoding %>>% pipe_model
 graph_learner <- GraphLearner$new(graph)
 split <- rsmp("holdout")
 
 #Test
-f <- file("EMMA_package/tests/logs/vim_irmi.txt", open = "a")
+f <- file("EMMA_package/tests/logs/softimpute.txt", open = "a")
 task <- mlr3oml::OMLTask$new(id = i)
 task <- task$task
 
@@ -37,19 +38,21 @@ sink(f, type = "output")
 sink(f, type = "message")
 options(warn = 1)
 
+try({
 rr <- resample(task, graph_learner, split)
+})
 
 if(exists("rr")){
 write(file = f, "\n")
 rr$print()
 write(file = f, "\n")
-rr$score()
+print(rr$score())
 }
 
 write(file = f, "\n\n")
 sink()
 sink()
 rm(list = ls())
-})
+
 }
 
