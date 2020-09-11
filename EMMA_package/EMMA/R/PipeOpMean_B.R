77c00f6accce2656cccd5d8a851b3dfbdfa9d7f2
okcze <piotr.phitz@gmail.com>
2020-09-11 16:56:22 +0200

    approach B: mean imputation

---
+++ EMMA_package/EMMA/R/PipeOpMean_B.R
@@ -0,0 +1,44 @@
+#' @title PipeOpMean_B
+#'
+#' @name PipeOpMean_B
+#'
+#' @description
+#' Impute numerical features by their mean in approach B (independently during the training and prediction phase). 
+#'
+#' @section Input and Output Channels:
+#' Input and output channels are inherited from \code{\link{PipeOpImpute}}.
+#'
+#' @section Parameters:
+#' The parameters include inherited from [`PipeOpImpute`], as well as: \cr
+#' \itemize{
+#' \item \code{id} :: \code{character(1)}\cr
+#' Identifier of resulting object, default \code{"imput_mean_B"}.
+#'}
+#'
+#' @export
+
+
+PipeOpMean_B <-  R6::R6Class("Mean_B_imputation",
+                             inherit = PipeOpImpute,
+                             public = list(
+                               initialize = function(id = "impute_mean_B", param_vals = list()) {
+                                 super$initialize(id, param_vals = param_vals, feature_types = c("numeric", "integer"))
+                                 }
+                               ),
+                             private = list(
+                                 
+                                 .train_imputer = function(feature, type, context){NULL},
+                                 
+                                 .impute = function(feature, type, model, context){
+                                   men = mean(feature, na.rm = TRUE)
+                                   if (type == "integer") {
+                                     men = as.integer(round(men))
+                                   }
+                                   feature[is.na(feature)] = men
+                                   feature
+                                   }
+                                 )
+                             )
+
+mlr_pipeops$add("impute_mean_B", PipeOpMean_B)
+
