efbe7bb92c4c24370b421a98b3e7ee8e3a263ffb
jjanborowka <janborowka7@gmail.com>
2020-10-11 19:34:47 +0200

    grf

--- EMMA_package/LearnerClassifGrf.R
+++ EMMA_package/LearnerClassifGrf.R
@@ -1,9 +1,10 @@
+
+
 LearnerClassifGrf = R6::R6Class("LearnerClassifGrf",
                                    inherit = LearnerClassif,
 
                                    public = list(
-                                     #' @description
-                                     #' Creates a new instance of this [R6][R6::R6Class] class.
+
                                      initialize = function() {
                                        ps = ParamSet$new(
                                          params = list(
@@ -12,53 +13,72 @@ LearnerClassifGrf = R6::R6Class("LearnerClassifGrf",
                                          )
                                        )
 
-                                       # If you change any defaults for mlr3 compared to the underlying learner,
-                                       # specify them here
+
                                        ps$values = list()
 
                                        super$initialize(
-                                         # see the mlr3book for a description: https://mlr3book.mlr-org.com/extending-mlr3.html
+
                                          id = "classif.grf",
-                                         packages = "misaem",
+                                         packages = "grf",
                                          feature_types = c("numeric","logical","integer",'factor'),
                                          predict_types = "response",
                                          param_set = ps,
-                                         properties = c( "twoclass", "multiclass")
+                                         properties = c( "twoclass")
 
                                        )
                                      }
 
 
-                                     # <Add method for importance, if learner supports that>
-                                     # <See mlr3learners.randomForest for an example>
 
-                                     # <Add method for oob_error, if learner supports that.>
 
                                    ),
 
                                    private = list(
                                      .train = function(task) {
                                        # data preperation
-                                       data_to_train <- as.data.frame(task$data(cols = task$feature_names))
+                                       data_to_train <- as.data.frame(task$data(cols=task$feature_names))
                                        target <- as.data.frame(task$data(cols = task$target_names))
 
                                        target <- target[,task$target_names,drop=T]
+
+                                       # Data encoding
+                                       vars <- colnames(data_to_train)
+
+                                       encoder <- recipe(data_to_train) %>% update_role(all_of(vars), new_role = "predictor") %>% step_dummy(all_predictors(),one_hot = F)
+
+                                       encoder_model <- prep(encoder,training =data_to_train)
+
+                                       self$state = list(
+                                         dummy_encode = encoder_model
+                                       )
+
+                                       data_to_train<- bake(encoder_model, data_to_train)
+
+
 
                                        if(class(target)=='factor'){
                                          self$state$levels <- levels(target)
                                          target <- as.integer(target)-1
                                        }
-                                       data_to_train<- cbind(data_to_train,target)
+
 
                                        # training
-                                       misaem::miss.glm(as.formula(paste0('target','~.')),data_to_train)
+                                       grf::boosted_regression_forest(data_to_train,target)
 
                                      },
 
                                      .predict = function(task) {
                                        data_to_predict <-  as.data.frame(task$data(cols = task$feature_names))
+                                       data_to_predict<- bake(self$state$dummy_encode,data_to_predict)
+
+
+
                                        pred <- predict(self$model,(data_to_predict))
-                                       pred <- self$state$levels[pred+1]
+
+
+
+
+                                       pred <- self$state$levels[ifelse(round(pred)<0.5,1,2)]
                                        PredictionClassif$new(task = task,response = pred)
                                      }
                                    )
@@ -69,13 +89,24 @@ LearnerClassifGrf = R6::R6Class("LearnerClassifGrf",
 
 
 
+
+
+
+
+
+
+
+
+
+
+
  d <- LearnerClassifGrf$new()
 gr <-   d
 grlr <- GraphLearner$new(gr)
 
-w<- resample(task,grlr,rsmp('holdout'))
+w<- resample(task,grlr,rsmp('cv',folds=5))
 
-
+w$aggregate(msr('classif.acc'))
 #
 # library(mlr3oml)
 # for (i in tasks$task.id){
@@ -84,7 +115,7 @@ w<- resample(task,grlr,rsmp('holdout'))
 # gr <-  d
 # grlr <- GraphLearner$new(gr)
 #
- task <- mlr3oml::OMLTask$new(id = 55)
+ task <- mlr3oml::OMLTask$new(id = 3704)
  task <- task$task
 #
 # tryCatch({
